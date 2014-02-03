550b0dab4287e4f4c94c6d61635729dbe76e1bec
Typetext <typecraft@hotmail.com>
2014-02-03 20:53:43 +0100

    Update Prueba
    Miguel primer aviso

--- Eclipse Tutorial/src/Prueba.java
+++ Eclipse Tutorial/src/Prueba.java
@@ -6,7 +6,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method
 	System.out.println("Carlos estoy urgandote en tus proyectos be careful");
-		System.out.println("Bronce V deja la programacion y vete a pescar");
+		System.out.println("Miguel si ves esto no me toques los parentesis");
 	
 	//quiero una estrellita por esta gran aportacion a tu proyecto =D
 	}
