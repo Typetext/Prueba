dd16b21d61cf0efa27369d536c130f6724a77cc4
Arux86 <alb3rto86@hotmail.com>
2014-02-07 19:06:27 +0100

    nuevo comentario
    añado un pantallazo

--- Eclipse Tutorial/src/Prueba.java
+++ Eclipse Tutorial/src/Prueba.java
@@ -7,6 +7,8 @@ public class Prueba {
 		// TODO Auto-generated method
 	System.out.println("Carlos estoy urgandote en tus proyectos be careful");
 		System.out.println("Miguel si ves esto no me toques los parentesis");
+		
+	System.out.println("keep calm & preguntar a carlos");
 	
 	//quiero una estrellita por esta gran aportacion a tu proyecto =D
 	}
